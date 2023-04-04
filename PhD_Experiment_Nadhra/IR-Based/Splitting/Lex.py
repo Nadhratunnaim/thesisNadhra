5f1f52ca411bc547aadf7be81585b36b1facea33
Nadhra_Nasarudin <nn653@york.ac.uk>
2023-04-05 07:19:45 +0800

    Delete PhD_Experiment_Nadhra/IR-Based directory

--- PhD_Experiment_Nadhra/IR-Based/Splitting/Lex.py
+++
@@ -1,9 +0,0 @@
-with open("inputdata.txt", "r") as f:  # opens inputdata txt file and reads contents.
-    s = f.read()
-for i in range(len(s)):  # for the length of the inputdata
-    if s[i].isalnum():
-        print(s[i], end="")  # prints string in one line
-    else:
-        if s[i - 1].isalnum():  # prints lexemes in newline
-            print()
-        print(s[i])
--- PhD_Experiment_Nadhra/IR-Based/Splitting/README.md
+++
@@ -1,4 +0,0 @@
-# LexicalAnalyzer
-A simple Lexical Analyzer that separates lexemes by white space and special characters.
-
-Lex reads input from inputdata.txt file
